8104ea1323ff23956ac48913f4a4fd86f6958ad2
greene80501 <152511573+greene80501@users.noreply.github.com>
2024-12-24 17:08:57 -0700

    Delete Lamma-3B_fine_tuned directory

--- Lamma-3B_fine_tuned/ahhh.py
+++
@@ -1,8 +0,0 @@
-from transformers import AutoTokenizer, AutoModelForCausalLM
-
-tokenizer = AutoTokenizer.from_pretrained(
-    "openlm-research/open_llama_3b", cache_dir="d:/Lamma-3B_fine_tuned"
-)
-model = AutoModelForCausalLM.from_pretrained(
-    "openlm-research/open_llama_3b", cache_dir="d:/Lamma-3B_fine_tuned"
-)
